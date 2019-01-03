daaa9cd79f30f4c12e002a76e5d91f294e7dbd8d
Su <suce126@126.com>
2019-01-03 23:47:44 +0100

    adding db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DAATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl:true,
     }
 });
